452b608809b603b4688639974d8231cc2f296ce1
amit <[amirana14325@gmail.com]>
2021-04-08 00:43:27 +0530

    [Amit Rana]Added::insrt data into table for address book

--- welcomeMassage.sql
+++ welcomeMassage.sql
@@ -3,7 +3,9 @@
 
 create database AddressBookServiceDB
 use AddressBookServiceDB
+
 --[UC2]
+
 create table AddressBook
 (
     first_name varchar(30) not null,
@@ -14,4 +16,15 @@ create table AddressBook
     zip int,
     phone_number varchar(10) not null
 );
+
+--[UC3]
+
+insert into AddressBook values
+('Amit','Rana','Awas vikas colony.','Rishikesh','UK',265356,'8979325434'),
+('Sumit','Semwal','kali mandir.','Ruderperyag','UK',548512,'9876543210'),
+('Naman','Dhiyani','puliya.','Devperyag','UK',546522,'8976543210'),
+('Aman','Rawat','jhanda choak.','Kotdwar','UK',985151,'8755423025'),
+('Saurabh','Sajwan',' ghantagher.','Dehradun','UK',26232,'7856235620'),
+('Gaurabh','Butola','kotewser.','Ruderperyag','UK',895154,'9875141545');
+select * from AddressBook
 
